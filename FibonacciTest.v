16aa2de3183eeae7813c0a38b220478112dbac9c
BYee7127 <byee7127@hotmail.com>
2018-09-14 22:24:09 -0600

    Changed bits, starting flushing out code (still commented)

--- FibonacciTest.v
+++ FibonacciTest.v
@@ -140,29 +140,31 @@ endmodule
 module FSM(clk, R, data, cath, an,
 	state, r1, r2, rout);
 	
-	input clk, R;			// clock and reset
-	input [3:0] data;
-	output [1:7] cath;
-	output [3:0] an;
+	input clk, R;				// clock and reset
+	input [3:0] data;			// outside data - immediate values, test?
+	output [1:7] cath;		// cathode
+	output [3:0] an;			// anode - the odes are for the display
 	
-	output reg [3:0] state, r1, r2;
-	output [3:0] rout;
+	output reg [15:0] state, r1, r2;
+	output [15:0] rout;		// output of the ALU - also known as C or ALUbus
 	
 	reg reset;
-	reg [1:0] aluc;
-	// reg [3:0] state;
-	// reg [3:0] r1, r2, rout;
-	wire [3:0] aluout;		// aluout is the output of the ALU - in this case C
+	reg [1:0] aluc;			// opcode
+	reg [15:0] tempVal;		// value to pose as the ALUbus input for the registers when they're enabled (?)
 	
-	parameter ADD = 2'b00, OR = 2'b01, NOT = 2'b10, XOR = 2'b11;
+	parameter ADD = 4'b0101;
 	
 	initial
 		begin
-			state = 4'b0000;
-			r1 = 4'b0000;
-			r2 = 4'b0000;
+			state = 16'b0000_0000_0000_0000;
+			r1 = 16'b0000_0000_0000_0000;
+			r2 = 16'b0000_0000_0000_0000;
+			rout = 16'b0000_0000_0000_0000;			// instantiate to eliminate latches
+			tempVal = 16'b0000_0000_0000_0001;		// make it equal to one
 		end
 	
+	// for each rising positive edge of the clock
+	// check to see if the reset button has been pressed
 	always @(posedge clk)
 		begin
 			if(R == 1'b1)
@@ -174,17 +176,26 @@ module FSM(clk, R, data, cath, an,
 	always @(posedge clk)
 		begin
 			if(reset == 1'b1)
+				// checks to see if the reset button has been pressed
 				state = 4'b0000;
 			else
 				begin
-					state = state + 4'b1;
+					state = state + 4'b1;		// increase the state by one
 					if(state > 4'b1001)
-						state = 4'b1001;		// keep it at state 9
+						state = 4'b1001;		// keep it at the last state?
 
 					case(state)
-						1:		// reads the data from an external source and loads it into r1
-							r1 = data;
-						2: // loads the number 3 into r2
+						1:		// load values of registers 0 and 1
+							begin
+							r1 = RegBank reg(tempVal, 16'b0000_0000_0000_0001, clk, R);		// r0
+							r2 = RegBank reg(tempVal, 16'b0000_0000_0000_0010, clk, R);		// r1
+							
+							// I don't know if this is the right syntax, but that's how I imagine it
+							// may have something to do with all the outputs but I dunno
+							// regEnable works by flipping the corresponding bit to the register
+							// for example
+							end
+						2: // adds the two registers and stores it to 
 							r2 = 4'b0011;
 						3: // add r1, r2 and store to rout
 							aluc = ADD;
@@ -202,8 +213,10 @@ module FSM(clk, R, data, cath, an,
 							aluc = XOR;
 						default:
 							begin
-								r1 = 4'b0000;
-								r2 = 4'b0000;
+								// just set them to 0 if something happens
+								r1 = 16'b0000_0000_0000_0000;
+								r2 = 16'b0000_0000_0000_0000;
+								rout = 16'b0000_0000_0000_0000;
 							end
 					endcase
 				end

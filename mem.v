59aff72335d4f72ac364c70f97d65809b19f5c1c
BYee7127 <byee7127@hotmail.com>
2018-09-26 16:03:54 -0600

    Added initalize memory (not sure what to do next), added some text in GamePlan, added commandList.txt

--- mem.v
+++ mem.v
@@ -12,21 +12,27 @@ module mem
 );
 
 	// Declare the RAM variable
+	// recall that ** means raise to the power of
+	// 16 bit word and 512 addr_width (2^10), which equals one block
 	reg [DATA_WIDTH-1:0] ram[2**ADDR_WIDTH-1:0];
+	
+	// initializing memory
+	initial begin
+		// use $readmemb if want to read in binarary
+		// right now, reading in hex
+		$readmemh("commandList.txt", ram);
+	end
 
 	always @ (posedge clk)
 	begin
 		// Write - if we (write enable) = 1, then you wanna write data
 		if (we)
-			ram[write_addr] <= data;
+			ram[write_addr] <= data;		// write the data into the ram at a specific address
 
-		// Read (if read_addr == write_addr, return OLD data).	To return
-		// NEW data, use = (blocking write) rather than <= (non-blocking write)
-		// in the write assignment.	 NOTE: NEW data may require extra bypass
-		// logic around the RAM.
-		
-		// two ways of reading RAM
-		// best way is to read from a txt file, where it has preset instructions to read into memory
+		// Read (if read_addr == write_addr, return OLD data).
+		// To return NEW data, use = (blocking write) rather than <= (non-blocking write)
+		// in the write assignment.
+		// NOTE: NEW data may require extra bypass logic around the RAM.
 		q <= ram[read_addr];
 	end
 

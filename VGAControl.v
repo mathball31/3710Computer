a2de33d0331b01f03ee1d5fb1282759abd7f1fc4
BYee7127 <byee7127@hotmail.com>
2018-11-06 10:55:02 -0700

    Added 2 new files for VGA

---
+++ VGAControl.v
@@ -0,0 +1,15 @@
+
+
+/*
+	Generate timing pulses at the right time
+	Hsync, vsync, bright, hcount, vcount
+	All about timing
+	Doesn't draw anything
+	
+	640 x 480 pixel screen
+*/
+module VGAControl ();
+
+
+
+endmodule
---
+++ bitGen.v
@@ -0,0 +1,12 @@
+
+/*
+	Based on bright, hcount, vcount, turn on the bits
+	Figures out the position and the color of the pixel
+	Draws the pixel
+*/
+module bitGen ();
+
+
+
+
+endmodule
